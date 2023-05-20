f82c7945a9c8f49480f9da9037f40e12867f2b9e
Jeremy Chone <jeremy.chone@gmail.com>
2023-05-20 10:15:55 -0700

    E01-C03 - Config

--- src/web/routes_static.rs
+++ src/web/routes_static.rs
@@ -1,11 +1,8 @@
-use std::fmt::format;
-
+use crate::config;
 use axum::handler::HandlerWithoutStateExt;
 use axum::http::{StatusCode, Uri};
 use axum::routing::{any_service, MethodRouter};
 use tower_http::services::ServeDir;
-
-const WEB_FOLDER: &str = "web-folder";
 
 // Note: Here we can just return a MethodRouter rather than a full Router
 //       since ServeDir is a service.
@@ -15,5 +12,8 @@ pub fn serve_dir() -> MethodRouter {
 		(StatusCode::NOT_FOUND, message)
 	}
 
-	any_service(ServeDir::new(WEB_FOLDER).not_found_service(handle_404.into_service()))
+	any_service(
+		ServeDir::new(&config().WEB_FOLDER)
+			.not_found_service(handle_404.into_service()),
+	)
 }
